ec15a37aa4676eac2b5e684e23c6ed26f56f9ccb
petro feniak <petro.fenyak@gmail.com>
2025-01-03 22:27:02 +0200

    added version control and checkstyle plugin

---
+++ src/test/java/project/footballmanager/FootballManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.footballmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
