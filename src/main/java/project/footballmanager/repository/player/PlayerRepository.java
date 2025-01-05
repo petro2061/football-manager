a3b7f8b0ed978172ed5c307e7e443762a0b60fb1
petro feniak <petro.fenyak@gmail.com>
2025-01-05 13:39:17 +0200

    added a special query to load a dependent entity

--- src/main/java/project/footballmanager/repository/player/PlayerRepository.java
+++ src/main/java/project/footballmanager/repository/player/PlayerRepository.java
@@ -1,6 +1,7 @@
 package project.footballmanager.repository.player;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,13 @@ import project.footballmanager.model.Player;
 
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
+
+    @Query("SELECT p FROM Player p LEFT JOIN FETCH p.team")
+    List<Player> findPlayerWithTeam();
+
+    @Query("SELECT p FROM Player p LEFT JOIN FETCH p.team WHERE p.id = :id")
+    Optional<Player> findPlayerWithTeamById(Long id);
+
     @Query("SELECT p FROM Player p LEFT JOIN FETCH p.team t WHERE t.id = :teamId")
     List<Player> findAllByTeamId(Long teamId);
 }
